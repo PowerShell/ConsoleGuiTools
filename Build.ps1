c8719084aed3f7c79607d96141ca605ed91bf3a4
corbob <30301021+corbob@users.noreply.github.com>
2020-03-03 23:21:08 -0800

    Use -noprofile (#46)
    This reduces load time on systems with a $profile that takes time to load (mine takes 5+ seconds)

--- Build.ps1
+++ Build.ps1
@@ -1,2 +1,2 @@
 Invoke-Build Build
-pwsh -c "Import-Module '$PSScriptRoot/module/Microsoft.PowerShell.GraphicalTools'; Get-Process | Out-GridView -PassThru"
+pwsh -noprofile -command "Import-Module '$PSScriptRoot/module/Microsoft.PowerShell.GraphicalTools'; Get-Process | Out-GridView -PassThru"
