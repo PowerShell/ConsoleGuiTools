4ef499edce4865fba6240f5aa1e8939ca8ebb856
Charlie Kindel <charlie@kindel.com>
2020-09-27 08:53:14 -0700

    updated build to also run ocgv

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,3 @@
 Invoke-Build Build
-#pwsh -noprofile -command "Import-Module -verbose '$PSScriptRoot/module/Microsoft.PowerShell.GraphicalTools'; Get-Process | Out-GridView -PassThru"
+pwsh -noprofile -command "Import-Module -verbose '$PSScriptRoot/module/Microsoft.PowerShell.GraphicalTools'; Get-Process | Out-GridView -PassThru"
 pwsh -noprofile -command "Import-Module -verbose '$PSScriptRoot/module/Microsoft.PowerShell.ConsoleGuiTools'; Get-Process | Out-ConsoleGridView -OutputMode Single"
