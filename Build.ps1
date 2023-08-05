beba7740e93fe2e5c6710db878ce37db73f324db
Tig Kindel <tig@users.noreply.github.com>
2023-08-05 12:34:29 -0600

    Enhanced Build script to increment bulid number automatically

--- Build.ps1
+++ Build.ps1
@@ -3,31 +3,34 @@
 # To build them all leave -ModuleName off the `InvokeBuild` command (e.g. Invoke-Build Build).
 # To build only one, specify it using the -ModuleName paramater (e.g. Invoke-Build Build -ModuleName Microsoft.PowerShell.ConsoleGuiTools).
 $ModuleName = "Microsoft.PowerShell.ConsoleGuiTools"
-
-$ModulePath = "$PSScriptRoot/module/$ModuleName"
+$BuildPath = "$PSScriptRoot/module/$ModuleName"
+$PsdPath = "src/$ModuleName/$ModulePath/$($ModuleName).psd1"
 
 # Assume this is the first build
 $build = 0
 
-$psd1Content = Get-Content $($ModulePath + "/$($ModuleName).psd1") -Raw -ErrorAction SilentlyContinue
+$psd1Content = Get-Content $PsdPath -Raw -ErrorAction SilentlyContinue
 if ($psd1Content) {
     # Extract the ModuleVersion from the .psd1 content using regular expression
     if ($psd1Content -match "ModuleVersion\s+=\s+'(.*?)'") {
         $prevVersion = $Matches[1]
         $prevVersionParts = $prevVersion -split '\.'
         $build = [int]$prevVersionParts[3] + 1
-        $ModuleVersion = "{0}.{1}.{2}" -f $prevVersionParts[0], $prevVersionParts[1], $prevVersionParts[2]
-    } else {
-       throw "ModuleVersion not found in the old .psd1 file."
+        $ModuleVersion = "{0}.{1}.{2}.{3}" -f $prevVersionParts[0], $prevVersionParts[1], $prevVersionParts[2], $build
     }
-} else {
-    "No previous version found. Assuming this is the first build."
-    # Get the ModuleVersion using dotnet-gitversion
-    $prevVersion = "1.0.0"
-    $ModuleVersion = "$($prevVersion)"
+    else {
+        "No previous version found. Assuming this is the first build."
+        # Get the ModuleVersion using dotnet-gitversion
+        $prevVersion = "1.0.0.0"
+        $ModuleVersion = "$($prevVersion)"
+    }
+    "Rewriting $PsdPath with new ModuleVersion: $ModuleVersion"
+    $updatedpsd1Content = $psd1Content -replace "ModuleVersion\s+=\s+'([\d\.]+)'", "ModuleVersion = '$ModuleVersion'"
+    $updatedpsd1Content | Out-File -FilePath $PsdPath -Encoding ascii
 }
-
-"New Version: $ModuleVersion"
+else {
+    throw "$PsdPath not found."
+}
 
 "Buildihg $ModuleName..."
 Invoke-Build Build -ModuleName $ModuleName
@@ -39,10 +42,10 @@ if ($localRepository) {
     $localRepositoryPath = $localRepository | Select-Object -ExpandProperty SourceLocation
     # Un-publishing $ModuleName from local repository at $localRepositoryPath"
     Remove-Item "${localRepositoryPath}/${ModuleName}.{$ModuleVersion}.nupkg" -Recurse -Force -ErrorAction SilentlyContinue
+    "Publishing ${localRepositoryPath}/${ModuleName}.$ModuleVersion.nupkg to `local'"
+    Publish-Module -Path $BuildPath  -Repository 'local'
 }
 
-"Publishing $ModuleName... to `local` PSRepository"
-Publish-Module -Path $ModulePath  -Repository 'local'
 
 # Run what was built as a bit of test of:
 # - Scale: recursive ls of the project
@@ -50,5 +53,5 @@ Publish-Module -Path $ModulePath  -Repository 'local'
 # - SelectMultiple
 $testCommand = "Get-ChildItem -Recurse | Out-ConsoleGridView -Debug -OutputMode Multiple -Title 'Imported Modules' -Filter \.xml"
 "Running test in new pwsh session: $testCommand"
-pwsh -noprofile -command "Import-Module -verbose '$PSScriptRoot/module/$ModuleName'; $testCommand"
+pwsh -noprofile -command "Import-Module -verbose $BuildPath; $testCommand"
 "Test exited. Build complete."
