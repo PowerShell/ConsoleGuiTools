6bea0ddf2da60fc40f115ade44f3c9f8cf2e6d15
Andy Jordan <2226434+andyleejordan@users.noreply.github.com>
2025-01-31 13:56:20 -0800

    Use `RequiredResource` hashtable to specify PowerShell module versions

--- tools/installPSResources.ps1
+++ tools/installPSResources.ps1
@@ -6,8 +6,19 @@ param(
 )
 
 if ($PSRepository -eq "CFS" -and -not (Get-PSResourceRepository -Name CFS -ErrorAction SilentlyContinue)) {
-    Register-PSResourceRepository -Name CFS -Uri "https://pkgs.dev.azure.com/powershell/PowerShell/_packaging/powershell/nuget/v3/index.json"
+    Register-PSResourceRepository -Name CFS -Uri "https://pkgs.dev.azure.com/powershell/PowerShell/_packaging/PowerShellGalleryMirror/nuget/v3/index.json"
 }
 
-Install-PSResource -Repository $PSRepository -TrustRepository -Name InvokeBuild
-Install-PSResource -Repository $PSRepository -TrustRepository -Name platyPS
+# NOTE: Due to a bug in Install-PSResource with upstream feeds, we have to
+# request an exact version. Otherwise, if a newer version is available in the
+# upstream feed, it will fail to install any version at all.
+Install-PSResource -Verbose -TrustRepository -RequiredResource  @{
+    InvokeBuild = @{
+        version = "5.12.1"
+        repository = $PSRepository
+      }
+    platyPS = @{
+        version = "0.14.2"
+        repository = $PSRepository
+    }
+}
